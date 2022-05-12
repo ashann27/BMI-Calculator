4fa11e7011238b7318650d488b37263e06f29d9d
Alex Shannon <alexwshannon@gmail.com>
2022-05-12 21:59:25 +0900

    added suggestions

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,4 +1,13 @@
 weight = input('体重を入力してください')
 height = input('センチ単位身長を入力してください')
 bmi = int(weight)/((int(height)/100)**2)
-print('Your BMI is {}'.format(round(bmi, 1)))
+print('あなたのBMIは {}'.format(round(bmi, 1)))
+
+if bmi < 18.5:
+    print ('あなたのBMIはやせです。もう少し食べてください！')
+
+elif bmi > 25:
+    print ('あなたのBMIは肥満です。ダイエットしてください！')
+
+else:
+    print ('あなたのBMIは標準です。食事はそのままで続けてください！')
