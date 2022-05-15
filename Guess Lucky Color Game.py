693586474c4f4488a9a705f35533fbe56045bd6f
Alex Shannon <alexwshannon@gmail.com>
2022-05-15 10:17:23 +0900

    changed list of objects(?) into list of strings

--- Guess Lucky Color Game.py
+++ Guess Lucky Color Game.py
@@ -1,23 +1,23 @@
 import random
 
-def guess_lucky_color():
-    colors = [pink, red, blue, green]
-    lucky_color = random.choice(colors)
-    print('今回、あなたのラッキーカラーはなんでしょうか！\n')
+colors = ['pink', 'red', 'blue', 'green']
+lucky_color = random.choice(colors)
+print('今回、あなたのラッキーカラーはなんでしょうか！\n')
 
 for i in range(len(colors)):
-    print('今選択できる色は{}'.format(colors))
-    answer = input('上の{}色から一つを入力してください\n\n'.format(len(colors)))
-    answer.lower()
-    if answer not in colors:
-        print('{}はリストに入ってないみたいです。スペルを確認してまた入力してみてください\n'.format(answer))
-        continue
+        print('今選択できる色は{}'.format(colors))
+        answer = input('上の{}色から一つを入力してください\n\n'.format(len(colors)))
+        answer.lower()
+        if answer not in colors:
+            print('{}はリストに入ってないみたいです。スペルを確認してまた入力してみてください\n'.format(answer))
+            continue
 
-    elif answer != lucky_color:
-        colors.remove(answer)
-        print('{}じゃないです！また当たてみてください。\n'.format(answer))
-        continue
+        elif answer != lucky_color:
+            colors.remove(answer)
+            print('{}じゃないです！また当たてみてください。\n'.format(answer))
+            continue
 
-    else:
-        print('正解！')
-        break
+        else:
+            print('正解！')
+            break
+
