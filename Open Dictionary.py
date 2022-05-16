e4aeadb1649c80a64f7646b7fe9c91a8c88d0705
Alex Shannon <alexwshannon@gmail.com>
2022-05-16 15:31:44 +0900

    added menu and corrected test yourself bugs

--- Open Dictionary.py
+++ Open Dictionary.py
@@ -29,38 +29,58 @@ def list_all_words():
         print('{} - {} \nHint: {}'.format(key, value[0], value[1]))
 
 def test_yourself():
-
+    playing = True
+    if not bool(dictionary):
+        print('\nまだ辞書に単語が入ってないです！まずは単語をつかしてください\n')
+        playing = not playing
     points = 0
     incorrect_words = {}
-    playing = true
+    while playing == True:
+        for key, value in dictionary.items():
 
-    for key, value in dictionary.items():
-        answer = input('{}はなんでしょうか？\n'
-          '単語を入力してください。また、「h」ヒント　「p」パス  「q」やめる'.format(value[0]))
+                still_trying = True
 
-        if answer == 'h':
-            print('\n Hint:{}'.format(value[1]))
+                while still_trying == True:
+                    answer = input('{}はなんでしょうか？\n'
+                        '単語を入力してください。また、「h」ヒント　「p」パス  「q」やめる\n'.format(value[0]))
+                    answer = answer.lower()
+                    if answer == key:
+                        print('正解！')
+                        points  += 1
+                        still_trying = not still_trying
+                    elif answer == 'h':
+                        print('\nHint:{}\n'.format(value[1]))
+                    elif answer == 'p':
+                        print('答えは{}でした'.format(key))
+                        incorrect_words[key] = None
+                        still_trying = not still_trying
+                    elif answer == 'q':
+                        still_trying = not still_trying
+                        playing = not playing
 
-        elif answer == 'p':
-            print('答えは{}でした'.format(key))
-            incorrect_words[key] = None
-        elif answer == 'q':
-            playing=false
-        else:
-            print(answer + 'ではないです')
-    print('\nScore: {}/{}'.format(points, len(dictionary)))
-    print('Incorrect Words:')
-    for key in incorrect_words:
-        print(key)
+                    else:
+                        print(answer + 'ではないです')
+        print('\n点数: {}/{}'.format(points, len(dictionary)))
+        print('間違った単語:')
+        for key in incorrect_words:
+            print(key)
+        playing = not playing
 
+while True:
+    cmd = input("""\nオープン辞書へようこそ!
+1) テスト　
+2) 単語を追加して
+3) 単語を削除して
+4) 追加した単語の発表
+5) やめる\n""")
 
-
-
-
-    
-
-add_word()
-list_all_words()
-remove_word()
-list_all_words()
-test_yourself()
+    if cmd == '1':
+        test_yourself()
+    elif cmd == '2':
+        add_word()
+    elif cmd == '3':
+        remove_word()
+    elif cmd == '4':
+        list_all_words()
+    elif cmd == '5':
+        break
