9bd304f7c8a093b6cf3b82f8c45ba2e0e4b9fe7e
Alex Shannon <alexwshannon@gmail.com>
2022-05-19 21:43:08 +0900

    creted character, player, enemy, menu and fight classes/methods

--- Cool Fighter.py
+++ Cool Fighter.py
@@ -2,25 +2,50 @@ import random
 from abc import ABC, abstractmethod
 
 class Character(ABC):
-    def __init__(self, hp, attack, defense):
+    def __init__(self, level):
         self.hp = 0
-        self.attack = 0
-        self.defense = 0
-
+    #add defense later
     #add item slot later
     pass
 
 class Player (Character):
-    ZZ#level up method
+    def __init__(self):
+        super().__init__(name, level)
+        self.level = 1
+        self.hp = level * 10
+        self.attack = level * 3
+        self.exp = 0
+
+    #level up method
         # player levels up every time they increase exp by 2, scales so you never have to beat more than 5 enemies to level up
         # exp gained is based on current level x enemy level bonus
 
+class Enemy (Character):
 
-# foes class
-# enemy hp is based on a range around players attributes
-# item generator, use word generator and make a sentence that autofills effects and name for an item. affect is random stat buff/nerf
+    def __init__(self):
+        super().__init__(level)
+        self.level = level
+        self.hp = level * random.randint(7,12)
+        self.attack = level * random.randit(3)
 
-# item class
+        enemy_types = ['ゴキブリ','へび','ウルフ','ヒューマン','さる','ゴリラ','サメ','白クマ','カバ']
+        self.name=enemy_types[level]
+
+        if hp < level * 9:
+            self.title = '弱い'
+            self.exp = attack + (attack * .7)
+        elif hp >= level * 11:
+            self.title = '強い'
+            self.exp = attack + (attack * 1.3)
+        else:
+            self.title = '普通'
+        #items?
+
+
+
+
+    def attack():
+        pass
 
 # item info generator method
 
@@ -37,3 +62,39 @@ class Player (Character):
 # and vice versa
 
 # character data can be saved to text file
+
+def start_game():
+    player = Player()
+    while True:
+        enemy = Enemy(player.level)
+        cmd = input('{} {}が来ました！戦おうとしてるらしい、どうしますか？\n'
+                '攻撃「A」　　　　逃げる「R」      やめる 「Q」\n'.format(enemy.title, enemy.name))
+        if cmd.lower() == 'a':
+            fight(player, enemy)
+
+        elif cmd.lower() == 'r':
+            print('ギリギリ逃げた！よかったね\n')
+            continue
+
+        elif cmd.lower() == 'q':
+            break
+
+        else: print('{} は選択に入ってません。また入力してください'.format(cmd))
+
+#def attack(Player, Enemy):
+
+def fight(player, enemy):
+    print('戦いが始まりました！')
+    turn = 1
+    while player.hp > 0 and enemy.hp > 0:
+        if turn % 2 == 1:
+            print('あなたが攻撃しました！')
+            enemy.hp -= player.attack
+            turn += 1
+        if turn % 2 == 1:
+            print('{}が{}攻撃をしました！'.format(enemy.name, enemy.title))
+            player.hp -= enemy.attack
+            turn += 1
+
+    if enemy.hp == 0:
+        print('{}{}を倒しました！{}expをもらいました。次のレベルは後{}expだけです！'.format(enemy.title, enemy.name, enemy.exp))
